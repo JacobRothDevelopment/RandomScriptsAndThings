999daac9a1fb47a4c98b4117b7d2f2939a7bb7bf
JacobRothDevelopment <rothy784715+github@gmail.com>
2023-04-26 06:59:13 -0400

    stuff

--- Math/CircleSims/AverageChordLength.js
+++ Math/CircleSims/AverageChordLength.js
@@ -1,14 +1,13 @@
 /* 
 find average length of a chord in a unit circle 
 
-
 assume: 
   1) the line from P1 to P2 is a chord on the unit circle centered on a point C at (0,0) 
   2) Θ is the angle from P1, C, and P2
   3) P1 is always translated to (1,0)
   4) Θ is uniformly distributed
 
-derived formulae. will not prove here
+derived formulae (will not prove here)
 d = √(2(1 - x2)) where x2 is the x component of P2
 putting this in terms of Θ:
   x2 = x1*cos(θ) − y1*sin(θ) // treating P2 as a copy of P1 being rotated by Θ degrees
@@ -19,13 +18,15 @@ putting this in terms of Θ:
 
 //#region CONST
 const numSamples = 100000;
+
 const average = (array) => array.reduce((a, b) => a + b) / array.length;
+const chordLength = (theta) => Math.sqrt(2 * (1 - Math.cos(theta)));
 //#endregion CONST
 
 let distances = [];
 for (let i = 0; i < numSamples; i++) {
   const theta = Math.random() * 360; // get random number from 0 - 360
-  const d = Math.sqrt(2 * (1 - Math.cos(theta)));
+  const d = chordLength(theta);
   distances.push(d);
   // console.log(`${theta} : ${d}`);
 }

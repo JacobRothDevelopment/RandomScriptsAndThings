698b6b1e49cd2d8375e6e9ba85f615e93352e35a
JacobRothDevelopment <rothy784715+github@gmail.com>
2024-08-18 13:53:53 -0400

    only click button if the video is over

--- degen/brainshark.js
+++ degen/brainshark.js
@@ -1,6 +1,14 @@
 // for arbiter sports clinic
 const AUTO_CLICK_SECONDS = 10;
+const ALLOW_DEBUG = false;
 
 window.setInterval(() => {
-  document.querySelector('#play-pause-button[aria-label=play]').click();
+  const timers = document.querySelectorAll('bsk-mediasync-time-display');
+  if (ALLOW_DEBUG) console.log('timers', timers);
+  const timeElapsed = timers[0].innerText;
+  const totalTime = timers[1].innerText;
+
+  if (timeElapsed >= totalTime) {
+    document.querySelector('#play-pause-button[aria-label=play]').click();
+  }
 }, AUTO_CLICK_SECONDS * 1000);
